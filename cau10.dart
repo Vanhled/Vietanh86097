c1581da001847062db288fef47870e5fdd038c2c
Vanhled <125440878+Vanhled@users.noreply.github.com>
2023-02-15 14:56:15 +0700

    Add files via upload

---
+++ cau10.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+ void main() {
+   String str = "Hi Trinh Tran Viet Anh";
+   str = str.replaceAll(' ', ''); 
+   print("The length of the string is: ${str.length}"); 
+}
+
---
+++ cau9.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+void main() {
+  String str = "Trinh    Tran Viet Anh";
+  str = str.replaceAll(' ', ''); 
+  print(str); 
+}
